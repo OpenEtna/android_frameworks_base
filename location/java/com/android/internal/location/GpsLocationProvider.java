82c937bef25b7e5e7e6d3041e2c3fbfe41f83ec7
polytheus <polytheus@googlemail.com>
2010-08-16 14:05:24 +0200

    GPS: Fix GPS_STATUS_SESSION_BEGIN not received
    Change-Id: I272f662f93e6ab4fb046ccf0e2ca89fd6de8dd0f

--- location/java/com/android/internal/location/GpsLocationProvider.java
+++ location/java/com/android/internal/location/GpsLocationProvider.java
@@ -995,6 +995,13 @@ public class GpsLocationProvider extends ILocationProvider.Stub {
             }
         }
 
+        /* BUG workaround: the korean-v10t libloc-api does not send
+         * GPS_STATUS_SESSION_BEGIN (but does send GPS_STATUS_SESSION_END)
+         * so we emulate GPS_STATUS_SESSION_BEGIN when we receive the first location */
+        if( !mNavigating )
+          reportStatus(GPS_STATUS_SESSION_BEGIN);
+ 
+
         updateStatus(mStatus, svCount);
 
         if (mNavigating && mStatus == LocationProvider.AVAILABLE && mLastFixTime > 0 &&
